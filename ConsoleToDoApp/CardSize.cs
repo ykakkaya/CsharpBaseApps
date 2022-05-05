9526b2991e8224550ef63b908a0cccb0b331343d
Yakup Akkaya <100940437+ykakkaya@users.noreply.github.com>
2022-05-05 11:41:04 +0300

    ToDoApp
    TODO uygulaması  Uygulamada olan özellikler aşağıdaki gibidir.
    Kart Ekle
    Kart Güncelle
    Kart Sil
    Kart Taşı
    Board Listeleme

---
+++ ConsoleToDoApp/CardSize.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleToDoApp
+{
+    public enum CardSize
+    {
+        XS=1,
+        S,
+        M,
+        L,
+        XL
+    }
+}
---
+++ ConsoleToDoApp/Line.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace ConsoleToDoApp
+{
+    public enum Line
+    {
+        TODOLine = 1,
+        INPROGRESSLine,
+        DONELİNE
+    }
+}
