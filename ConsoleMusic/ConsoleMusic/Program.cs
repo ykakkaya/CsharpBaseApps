fa24b66ecaf294754a9aa21fde231f5020b99631
Yakup Akkaya <100940437+ykakkaya@users.noreply.github.com>
2022-05-30 09:28:16 +0300

    media player ile ses dosyası çalıştırma
    tek satırlık komutla media player ile ses dosyası çalıştırma

---
+++ ConsoleMusic/ConsoleMusic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+namespace ConsoleMusic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\produ\Desktop\theme-song-hd.wav");
+        }
+    }
+}
